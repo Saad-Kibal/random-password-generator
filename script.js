79dd06c587168ae446244d1ad5f0a6c75a084c35
Saad-Kibal <saadkibal58@gmail.com>
2024-10-29 16:45:45 +0100

    add few css updates

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+/*
+1. Get character types from checkboxes.
+2. Loop length times:
+   1. Pick random character from chosen types.
+   2. Add to password.
+3. Display password. */
+
 const lengthInput = document.getElementById('length');
 const upperInput = document.getElementById('upper');
 const lowerInput = document.getElementById('lower');
 const numberInput = document.getElementById('numbers');
 const symbolInput = document.getElementById('symbols');
-const generateBtn = document.getElementById('generate');
 const passwordDisplay = document.getElementById('password');
 
 const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -27,6 +33,10 @@ function generatePassword() {
     }
     let password = '';
     for (let i = 0; i < lengthInput.value; i++) {
+        if (lengthInput.value < 4 || lengthInput.value > 30) {
+            passwordDisplay.textContent = 'Length must be between 4 and 20!';
+            return;
+        }
         password += characterList.charAt(Math.floor(Math.random() * characterList.length));
     }
     passwordDisplay.textContent = password;
